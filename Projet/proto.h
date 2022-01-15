cfb37e83e24e1ffa347a12590ed89405405a1b50
Benjamin Engloo <engloo.benjamin@gmail.com>
2022-01-15 16:04:13 +0100

    v1

--- Projet/proto.h
+++
@@ -1,7 +0,0 @@
-#ifndef REQREP_H
-    #define REQREP_H
-    #include "reqRep.h"
-#endif  
-//-fct generation des requétes
-//-à chaque req ,on associera &fct de traitement qui genere une réponse
-//1 fct de selection traitement selon requete
