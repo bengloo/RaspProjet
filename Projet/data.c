4b5a556345fdb29322d605986be51f77650a79ce
Benjamin Engloo <engloo.benjamin@gmail.com>
2022-01-19 17:48:11 +0100

    makfill

--- Projet/data.c
+++ Projet/data.c
@@ -4,7 +4,7 @@ void reqTOstr(req_t *req,buffer_t buff){
     sprintf(buff,"%3d:%d:%s",req->idReq,req->lgreq,req->msgReq);
 }
 void strTOreq(req_t *req,buffer_t buff){
-    sscanf(buff,FMT_REQ,req->idReq,req->lgreq,req->msgReq);
+    sscanf(buff,FMT_REQ,&req->idReq,&req->lgreq,req->msgReq);
 }
 
 void repTOstr(rep_t *req,buffer_t buff){
