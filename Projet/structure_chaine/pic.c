9a7f8b559d82a5ddddf2e1584b11716e2b73447d
Laurelene <58777491+Laurelene@users.noreply.github.com>
2022-02-04 21:56:30 +0100

    update pic (non fonctionnel)

--- Projet/structure_chaine/pic.c
+++ Projet/structure_chaine/pic.c
@@ -5,32 +5,34 @@
 #include <unistd.h>
 #include <string.h>
 
-#define Y_PIX 4	
+#define Y_PIX 4 
 #define X_PIX 3
 
 
 void picToString(char ** pic, char ch[1000000], int mon_score, int son_score);
-void stringToPic(char ** pic, char ch[1000000], int *mon_score, int *son_score);
+void stringToPic(char ** pic2, char ch[1000000], int *mon_score2, int *son_score2);
 
     
     
 char **empty_picture(char empty_char) {
-	char **pic;
-	pic = malloc(sizeof(char *) * Y_PIX);
-	for (int i = 0; i < Y_PIX; ++i) {
-		pic[i] = malloc(sizeof(char *) * X_PIX);
-		for (int j = 0; j < X_PIX; ++j) {
-			pic[i][j] = empty_char;
-		}
-	}
-	return pic;
+    char **pic;
+    pic = malloc(sizeof(char *) * Y_PIX);
+    for (int i = 0; i < Y_PIX; ++i) {
+        pic[i] = malloc(sizeof(char *) * X_PIX);
+        for (int j = 0; j < X_PIX; ++j) {
+            pic[i][j] = empty_char;
+        }
+    }
+    return pic;
 }
 
     
 int main() {
     int mon_score=10;
-	int son_score=11;
-	char **pic = empty_picture(' ');
+    int son_score=11;
+    char **pic = empty_picture(' ');
+    char **pic2 = empty_picture(' ');
+    int mon_score2, son_score2;
     //pic[Y_PIC][X_PIC]
     //pic vaut soit c, soit o, soit x, soit espace 
 
@@ -55,11 +57,11 @@ int main() {
 
     printf("\n");
     for (int i = 0; i < Y_PIX; ++i) {
-		for (int j = 0; j < X_PIX; ++j) {
-			printf("%c", pic[i][j]);
-		}
+        for (int j = 0; j < X_PIX; ++j) {
+            printf("%c", pic[i][j]);
+        }
         printf("\n");
-	}
+    }
 
 
     //char ch[Y_PIX*X_PIX];
@@ -71,20 +73,36 @@ int main() {
 
     printf("\n la chaine est %s", ch);
 
+
+    stringToPic(pic2,ch, &mon_score2, &son_score2);
+
+    printf("\n mon score est %d", mon_score2);
+    printf("\n son score est %d", son_score2);
+
+    printf("\n");
+    for (int i = 0; i < Y_PIX; ++i) {
+        for (int j = 0; j < X_PIX; ++j) {
+            printf("%c", pic2[i][j]);
+        }
+        printf("\n");
+    }
+
+
+
     return 0;
 
 }
 
 
-void picToString(char ** pic, char ch[1000000], int mon_score, int son_score) {
+void picToString(char ** pic, char ch[1000000], int mon_score2, int son_score2) {
 
     char aux[10];
 
-    sprintf(ch, "%d", mon_score);  
+    sprintf(ch, "%d", mon_score2);  
     strcat(ch,";");
 
 
-    sprintf(aux, "%d", son_score);
+    sprintf(aux, "%d", son_score2);
     strcat(ch,aux);
     strcat(ch,";");
 
@@ -97,7 +115,7 @@ void picToString(char ** pic, char ch[1000000], int mon_score, int son_score) {
 
 
     for (int i = 0; i < Y_PIX; ++i) {
-		for (int j = 0; j < X_PIX; ++j) {
+        for (int j = 0; j < X_PIX; ++j) {
 
 
             if (j == 0 && i ==0)
@@ -112,6 +130,7 @@ void picToString(char ** pic, char ch[1000000], int mon_score, int son_score) {
                     sprintf(aux, "%d", occ);  
                     strcat(ch,aux);
                     strcat(ch,&flag);
+                    strcat(ch,";");
                 }
             
 
@@ -125,12 +144,13 @@ void picToString(char ** pic, char ch[1000000], int mon_score, int son_score) {
             }
         }
 
-	}
+    }
+
 
 }
 
 
-void stringToPic(char ** pic, char ch[1000000], int *mon_score, int *son_score) 
+void stringToPic(char ** pic2, char ch[1000000], int *mon_score, int *son_score) 
 {
     char aux[100];
     int pos=0;
@@ -150,7 +170,6 @@ void stringToPic(char ** pic, char ch[1000000], int *mon_score, int *son_score)
     *mon_score=value;
 
     // récupération son_score
-    int i;
     for(i=0; ch[pos]!=';';i++, pos++) {
         aux[i]=ch[pos];
     }
@@ -160,5 +179,34 @@ void stringToPic(char ** pic, char ch[1000000], int *mon_score, int *son_score)
     value = strtol(aux,&str,10);
 
     *son_score=value;
+
+    int occ, posOcc=0;
+    char occChaine[100];
+
+    int x=0,y=0;
+    printf("\n");
+    // récupération de pic
+    for(pos;ch[pos]!=';'; i++, pos++) {
+        // si c'est un chiffre
+        if(ch[pos]=='0' || ch[pos]=='1' || ch[pos]=='2' || ch[pos]=='3' || ch[pos]=='4' || ch[pos]=='5' || ch[pos]=='6' || ch[pos]=='7' || ch[pos]=='8' || ch[pos]=='9'){
+           // occChaine[posOcc]=ch[pos];
+           // posOcc++;
+           printf("j'ai un chiffre \n");
+        }
+
+        // si c'est une lettre 
+        else {
+           // posOcc=0;
+            // transformation de l'occurence 
+           // occ=strtol(occChaine,&str,10);
+            /*for(y;occ!=0;y++, occ--) {
+                for(x;occ!=0;x++, occ--) {
+                    pic2[y][x]=ch[pos];
+                }
+            } */
+            printf("j'ai une lettre \n");
+            
+        }
+    }
 
 }
