3af9d5bd309f6936f09b99f57820b7fcc705dec5
Benjamin Engloo <engloo.benjamin@gmail.com>
2022-03-05 18:08:05 +0100

    test

---
+++ test perif/test.c
@@ -0,0 +1,16 @@
+#include "/home/bengloo/Documents/LE3/rasp/wiringpi/wiringPi-36fb7f1/wiringPi/wiringPi.h"
+
+#define BTN 6
+#define BUZZER 1
+
+int main (void)
+{
+  wiringPiSetup () ;
+  pinMode (0, OUTPUT) ;
+  for (;;)
+  {
+    digitalWrite (BUZZER, HIGH) ; delay (500) ;
+    digitalWrite (BUZZER,  LOW) ; delay (500) ;
+  }
+  return 0 ;
+}
